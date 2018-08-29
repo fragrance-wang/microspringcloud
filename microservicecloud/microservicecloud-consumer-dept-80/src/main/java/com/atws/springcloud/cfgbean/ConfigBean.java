b9e2a3867133ec231cf6b92d1489ac0da87e2de8
wangshan <1872672106@qq.com>
2018-08-29 16:22:48 +0800

    ribbon客户端与eureka整合

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atws/springcloud/cfgbean/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atws/springcloud/cfgbean/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.atws.springcloud.cfgbean;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ public class ConfigBean {
 	
 	//相当于在spring的配置文件中添加类
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate(){
 		
 		return new RestTemplate();
